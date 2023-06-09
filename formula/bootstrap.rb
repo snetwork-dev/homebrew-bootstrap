ec2641b95b3492e63a2636274edf73b98ba7ab86
S-Network <59974875+snetwork-dev@users.noreply.github.com>
2023-06-09 15:37:54 +0200

    Update bootstrap.rb

--- formula/bootstrap.rb
+++ formula/bootstrap.rb
@@ -3,7 +3,11 @@ require_relative "../custom_download_strategy"
 class Bootstrap < Formula
   desc "S-Network Bootstrap test"
   homepage "https://github.com/snetwork-dev/bootstrap"
-  url "https://github.com/snetwork-dev/bootstrap/archive/refs/tags/0.1.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy 
+  url "https://api.github.com/repos/snetwork-dev/bootstrap/archive/refs/tags/0.1.tar.gz",
+      headers: [
+        "Accept: application/octet-stream",
+        "Authorization: bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
+      ]
   sha256 "5e469655b9586371f12d7f8c1aebf39e3706e23512e572e74bf3649ca77b23b9"
   version "0.1.0"
   license ""
