7659869b52fddbdf8929062eea7c7911275972f3
S-Network <59974875+snetwork-dev@users.noreply.github.com>
2023-06-09 16:24:51 +0200

    Update bootstrap.rb

--- formula/bootstrap.rb
+++ formula/bootstrap.rb
@@ -1,7 +1,7 @@
 class Bootstrap < Formula
   desc "S-Network Bootstrap test"
   homepage "https://github.com/snetwork-dev/bootstrap"
-  url "https://api.github.com/repos/snetwork-dev/bootstrap/releases/107827726/assets",
+  url "https://api.github.com/repos/snetwork-dev/bootstrap/tarball/0.1",
       headers: [
         "Accept: application/octet-stream",
         "Authorization: bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
