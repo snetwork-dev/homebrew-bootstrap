e453c155cfa89e21120e28da2e02730c0f252a93
S-Network <59974875+snetwork-dev@users.noreply.github.com>
2023-06-10 15:37:23 +0200

    Update bootstrap.rb

--- formula/bootstrap.rb
+++ formula/bootstrap.rb
@@ -3,7 +3,6 @@ class Bootstrap < Formula
   homepage "https://github.com/snetwork-dev/bootstrap"
   url "https://github.com/snetwork-dev/bootstrap"
   sha256 "5e469655b9586371f12d7f8c1aebf39e3706e23512e572e74bf3649ca77b23b9"
-  version "0.1.0"
   license ""
 
   def install
