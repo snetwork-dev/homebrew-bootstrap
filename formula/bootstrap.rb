0117a8388705f8b6cdbf6b633fb38eba3a0b0ba4
S-Network <59974875+snetwork-dev@users.noreply.github.com>
2023-06-10 17:53:00 +0200

    Update bootstrap.rb

--- formula/bootstrap.rb
+++ formula/bootstrap.rb
@@ -4,7 +4,7 @@ class Bootstrap < Formula
   url "https://github.com/snetwork-dev/bootstrap/archive/refs/tags/0.1.tar.gz",
       headers: [
         "Accept: application/octet-stream",
-        "Authorization: bearer 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"
+        "Authorization: bearer ENV['HOMEBREW_GITHUB_API_TOKEN']"
       ]
   version "0.1.0"
   license ""
