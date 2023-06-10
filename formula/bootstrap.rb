e4ef19a303e86428d7b8158d49549bdea8b6982e
S-Network <59974875+snetwork-dev@users.noreply.github.com>
2023-06-10 16:19:16 +0200

    Update bootstrap.rb

--- formula/bootstrap.rb
+++ formula/bootstrap.rb
@@ -3,7 +3,7 @@ require_relative "../lib/custom_download_strategy"
 class Bootstrap < Formula
   desc "S-Network Bootstrap test"
   homepage "https://github.com/snetwork-dev/bootstrap"
-  url "https://github.com/snetwork-dev/bootstrap", :using => GitHubPrivateRepositoryDownloadStrategy
+  url "https://github.com/snetwork-dev/bootstrap/", :using => GitHubPrivateRepositoryDownloadStrategy
   sha256 "5e469655b9586371f12d7f8c1aebf39e3706e23512e572e74bf3649ca77b23b9"
   version "0.1.0"
   license ""
