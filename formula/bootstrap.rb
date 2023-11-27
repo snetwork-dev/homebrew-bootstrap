3322c80562a32b5cf4d45231fab0f26d2d458f27
S-Network <59974875+snetwork-dev@users.noreply.github.com>
2023-11-27 22:06:00 +0100

    Update bootstrap.rb

--- formula/bootstrap.rb
+++ formula/bootstrap.rb
@@ -10,9 +10,12 @@ class Bootstrap < Formula
   def install
     if ENV['ZSH_VERSION'] != ""
       bin.install "scripts/sshmenu.zsh" => "sshmenu"
+      bin.install_symlink "#{prefix}/bin/sshmenu" => "sm"
       bin.install "scripts/test.zsh"
       bin.install_symlink "#{prefix}/bin/test.zsh" => "testsymlinknew"
     elsif ENV['BASH_VERSION'] != ""
+      bin.install "scripts/sshmenu.sh" => "sshmenu"
+      bin.install_symlink "#{prefix}/bin/sshmenu" => "sm"
       bin.install "scripts/test.sh"
       bin.install_symlink "#{prefix}/bin/test.sh" => "testsymlinknew"
     end
