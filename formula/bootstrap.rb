63d898f163c90834d1e4b22c0b898bcbd6c1385c
S-Network <59974875+snetwork-dev@users.noreply.github.com>
2023-11-28 10:46:02 +0100

    Update bootstrap.rb

--- formula/bootstrap.rb
+++ formula/bootstrap.rb
@@ -6,7 +6,6 @@ class Bootstrap < Formula
         "Authorization: Bearer #{ENV['HOMEBREW_GITHUB_API_TOKEN']}"
       ]
   license :cannot_represent
-  version "0.1"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   def install
