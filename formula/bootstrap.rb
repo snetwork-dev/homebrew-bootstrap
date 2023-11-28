ed19ba546a0edda196ec25b392a24388897ec5a3
S-Network <59974875+snetwork-dev@users.noreply.github.com>
2023-11-28 10:44:24 +0100

    Update bootstrap.rb

--- formula/bootstrap.rb
+++ formula/bootstrap.rb
@@ -1,11 +1,13 @@
 class Bootstrap < Formula
   desc "S-Network Bootstrap test"
   homepage "https://github.com/snetwork-dev/bootstrap"
-  url "https://github.com/snetwork-dev/bootstrap/archive/refs/tags/v0.1.1.tar.gz",
+  url "https://github.com/snetwork-dev/bootstrap/tarball/master",
       headers: [
         "Authorization: Bearer #{ENV['HOMEBREW_GITHUB_API_TOKEN']}"
       ]
   license :cannot_represent
+  version "0.1"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   def install
     if ENV['ZSH_VERSION'] != ""
